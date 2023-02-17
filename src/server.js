75aea662d335983f01c7560bf18394e90c7e715e
Joaoof <joaodeus400@gmail.com>
2023-02-17 08:33:51 -0300

    Saving users in memory(Headers)

--- src/server.js
+++ src/server.js
@@ -2,15 +2,31 @@ import http from 'node:http' // Exportar a const http
 
 // Criar um usuário (nome, email, senha)
 
+// Stateful - Onde os dados da aplicação são armazenados em mémoria
+
+// Como que o nosso front-end vai saber que o back devolveu uma respota em formtado de JSON?
+// => Cabeçalhos (Requisição/resposta) => Metadados
+
+const users = [] // Array vazio
+
 const server = http.createServer((req, res) => {
 
   const { method, url } = req
 
   if(method == 'GET' && url == '/users') {
-    return res.end("Listagem de usuários")
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
   }
 
   if(method == 'POST' && url == '/users') {
+
+    users.push({ // criação de um novo usuário pra dentro do array
+      id: 1,
+      name: "Jonh of God",
+      email: "Jonh@example.com"
+    })
+
     return res.end("Criação de usuários")
   }
 
